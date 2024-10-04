ee4373a7fabad07a1cc4c6197eb79ec56a1af6a3
kodxxl <xsrc@mail.ru>
2024-10-04 14:55:41 +0400

    Test

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go_test
 
-go 1.21
+go 1.23.2
+
+require github.com/kodxxl/modforimport v0.0.0-20241004104213-2b33e65f7cb4
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"fmt" 
 	"math/rand"
+	"github.com/kodxxl/modforimport/pack1"
 )
 
 func main() {
-	fmt.Println("My favorite number is", rand.Intn(10))
+	pack1.Add(1, 2)
+	fmt.Println("My favorite number is", pack1.Add(rand.Intn(10),rand.Intn(10)) )
 }
